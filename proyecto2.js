74608f054ed5f41e93a75e3603c50a38106b2abc
marta-canizares <marta.s.canizares@gmail.com>
2020-04-24 15:35:12 +0200

    first commit

---
+++ proyecto2.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+// VARIABLE GLOBAL //
+function imprimeConsola() {
+    console.log(articles)
+}
+
+imprimeConsola()
+
+
+// VARIABLE LOCAL // ARGUMENTO / PARAMETRO
+function imprimeConsola(articlesLocal) {
+    console.log(articlesLocal)
+}
+
+imprimeConsola(articles)
+
+
+// Imprimir en pantalla un titulo "Series" y una lista con los nombres.
+
+function imprimeSeries() {
+    for (let series of articles)
+}
+document.writw(`</h1>Series</1>`)
+
+document.writw(`Nombre:`)
+
+
+
+imprimeSeries(articles)
+
